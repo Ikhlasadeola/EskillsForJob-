5bc2b68f74e3a81567e6864d05b9141b2adbcba5
Ikhlasadeola <ikhlasadeola@gmail.com>
2024-11-05 12:06:33 -0800

    second set

--- eskills.js
+++ eskills.js
@@ -4,3 +4,64 @@ function area(width, length){
 }
 console.log(area(5,7))
 3;
+1.5;
+1 + 1;
+0.1 + 0.2;
+8-1;
+10*2;
+35/5;
+5/2;
+10 % 2;
+30 % 4;
+18.5 % 7;
+1 << 2;
+(1 + 3) * 2;
+
+Infinity;
+-Infinity;
+NaN;
+
+true;
+false;
+
+'abc';
+"Hello, world";
+
+!true;
+!false;
+
+1 === 1;
+2 === 1;
+
+1 !== 1;
+2 !== 1;
+
+1 < 10;
+1 > 10;
+2 <= 2;
+2 >= 2;
+
+"Hello" + "world!";
+
+"1, 2, " + 3;
+"Hello " + ["world", "!"];
+
+13 + !0;
+"13" + !0;
+
+"a" < "b";
+
+"5" == 5;
+null == undefined;
+
+"5" === 5;
+null === undefined;
+
+"This is a string".charAt(0);
+
+"Hello world".substring(0,5);
+
+"Hello".length;
+
+null;
+undefined;
